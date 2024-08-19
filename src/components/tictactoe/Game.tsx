cc5761f65f63f5a50149ffaff0216122ecc1fab0
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-08-19 16:46:21 +0200

    move css-files for games

--- src/components/tictactoe/Game.tsx
+++ src/components/tictactoe/Game.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Board from './Board';
-import '../../styles/tictactoe/TicTacToe.css';
+import './style/TicTacToe.css';
 import HomeButton from '../HomeButton'; // reusable home button
 
 const Game: React.FC = () => {
