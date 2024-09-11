8034ea3a41bf5ee09d9bcbf8521ed231ed973fd0
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2024-09-11 13:50:17 +0200

    update style greeting

--- src/components/ChristmasCard.tsx
+++ src/components/ChristmasCard.tsx
@@ -2,10 +2,18 @@ import '../styles/ChristmasCard.css';
 
 const ChristmasCard = () => {
   return (
+    <>
     <div className="christmas-card">
+      <div className='greeting-container'>
       <h1>God Jul!</h1>
       <p>önskar Anna och Oskar</p>
+      </div>
+      <div className='greeting-content'>
+        <p>Vi vill önska er en riktigt skön jul och ett gott nytt år.</p>
+        <p>Prova gärna något av våra spel, eller hitta ett recept på något gott att smaska på.</p>
+      </div>
     </div>
+    </>
   );
 };
 
